0de9a8438f19b05b67acb6939be06c07222ea042
Carlos Wimmer <coiote777@gmail.com>
2026-03-15 07:44:31 +0100

    Create Card structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { ArchiveIcon } from "lucide-react"
-
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react"
+import { Card } from "@/components/card"
 import { Section } from "@/components/section"
 
 export default function Home() {
@@ -19,9 +19,28 @@ export default function Home() {
           </Section.Header>
 
           <Section.Content>
-            <div>Card</div>
-            <div>Card</div>
-            <div>Card</div>
+            <Card.Root>
+              <Card.Header>
+                <Card.Number>ECO-001</Card.Number>
+                <Card.Title>Implement credit card</Card.Title>
+              </Card.Header>
+              <Card.Footer>
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <ThumbsUpIcon className="size-3" />
+                  <span className="text-sm">12</span>
+                </button>
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <MessageCircleIcon className="size-3" />
+                  <span className="text-sm">6</span>
+                </button>
+              </Card.Footer>
+            </Card.Root>
           </Section.Content>
         </Section.Root>
       </main>
---
+++ src/components/card.tsx
@@ -0,0 +1,66 @@
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface CardRootProps extends ComponentProps<"a"> {}
+
+function CardRoot({ className, ...props }: CardRootProps) {
+  return (
+    <a
+      href="/"
+      className={twMerge(
+        "bg-navy-700 border-[0.5px] border-navy-600 p-3 space-y-4 rounded-lg block",
+        className,
+      )}
+      {...props}
+    ></a>
+  )
+}
+
+interface CardHeaderProps extends ComponentProps<"div"> {}
+
+function CardHeader({ className, ...props }: CardHeaderProps) {
+  return (
+    <div className={twMerge("flex flex-col gap-2", className)} {...props}></div>
+  )
+}
+
+interface CardTitleProps extends ComponentProps<"div"> {}
+
+function CardTitle({ className, ...props }: CardTitleProps) {
+  return (
+    <span
+      className={twMerge("text-sm font-medium", className)}
+      {...props}
+    ></span>
+  )
+}
+
+interface CardNumberProps extends ComponentProps<"div"> {}
+
+function CardNumber({ className, ...props }: CardNumberProps) {
+  return (
+    <span
+      className={twMerge("text-xs text-navy-200", className)}
+      {...props}
+    ></span>
+  )
+}
+
+interface CardFooterProps extends ComponentProps<"div"> {}
+
+function CardFooter({ className, ...props }: CardFooterProps) {
+  return (
+    <div
+      className={twMerge("flex items-center gap-2", className)}
+      {...props}
+    ></div>
+  )
+}
+
+export const Card = {
+  Root: CardRoot,
+  Header: CardHeader,
+  Title: CardTitle,
+  Number: CardNumber,
+  Footer: CardFooter,
+}
